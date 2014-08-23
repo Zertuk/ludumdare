f4b047bfcb43f8b880639d76f21520d2e1a01e44
Justin <zertukis@gmail.com>
2014-08-23 16:08:20 -0500

    added new assets into game

--- js/load.js
+++ js/load.js
@@ -1,9 +1,18 @@
 var load_state = {
 	preload: function() {
+		game.load.image('background', 'assets/background.png');
+		game.load.image('platform', 'assets/platform.png');
+		game.load.spritesheet('shadow', 'assets/shadow.png');
+		game.load.spritesheet('light', 'assets/light.png');
+		game.load.image('box', 'assets/box.png');
+		game.load.image('emptybox', 'assets/emptybox.png');
+		game.load.image('spikes', 'assets/spikes.png');
+		game.load.image('fullbox', 'assets/fullbox.png');
 
 	},
 
 	create: function() {
+		this.game.state.start('play');
 
 
 	}
